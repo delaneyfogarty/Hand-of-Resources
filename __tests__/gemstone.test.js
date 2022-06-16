a46fa209a8e26e54d159789248b7ab83e7f02bc4
Delaney Fogarty <delaney.fk@gmail.com>
2022-06-16 16:02:57 -0700

    test passing for get all gemstones

--- __tests__/gemstone.test.js
+++ __tests__/gemstone.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Gemstone = require('../lib/models/Gemstone');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
