4ebe49e7d560c2d16c4959cc06f0902316c4aee4
Delaney Fogarty <delaney.fk@gmail.com>
2022-06-16 15:53:41 -0700

    table set up and app route established for gemtones

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,6 +2,7 @@
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
 DROP table if exists flowers;
+DROP table if exists gemstones;
 
 CREATE table flowers (
 	id BIGINT GENERATED ALWAYS AS IDENTITY,
@@ -15,3 +16,16 @@ INSERT INTO flowers (name, color) VALUES
 ('Daisy', 'White'),
 ('Tulip', 'Yellow'),
 ('Hydrangea', 'Blue');
+
+CREATE table gemstones (
+	id BIGINT GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR NOT NULL,
+	properties VARCHAR NOT NULL,
+	birth_stone BOOLEAN NOT NULL
+);
+
+INSERT INTO gemstones (name, properties, birth_stone) VALUES
+('Amethyst', 'Cleansing, Protective, Inspiring', true),
+('Citrine', 'Positivity, Abundance, Manifestation', true),
+('Labradorite', 'Safe Exploration, Intuition, Peaceful', false),
+('Thulite', 'Love, Optimism, Compassion', false);
