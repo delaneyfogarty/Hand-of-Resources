b25bc201278dae4bd24752d789d6292e7fbc218a
Delaney Fogarty <delaney.fk@gmail.com>
2022-06-17 09:43:54 -0700

    passing test for get ALL cats

--- __tests__/cats.test.js
+++ __tests__/cats.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Cat = require('../lib/models/Cat');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
--- sql/setup.sql
+++ sql/setup.sql
@@ -3,6 +3,7 @@
 
 DROP table if exists flowers;
 DROP table if exists gemstones;
+DROP table if exists cats;
 
 CREATE table flowers (
 	id BIGINT GENERATED ALWAYS AS IDENTITY,
@@ -29,3 +30,19 @@ INSERT INTO gemstones (name, properties, birth_stone) VALUES
 ('Citrine', 'Positivity, Abundance, Manifestation', true),
 ('Labradorite', 'Safe Exploration, Intuition, Peaceful', false),
 ('Thulite', 'Love, Optimism, Compassion', false);
+
+CREATE table cats (
+	id BIGINT GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR NOT NULL,
+	color VARCHAR NOT NULL,
+	favorite_food VARCHAR NOT NULL,
+	age INT NOT NULL
+);
+
+INSERT INTO cats (name, color, favorite_food, age) VALUES
+('Boi George', 'Tuxedo', 'Tuna', 1),
+('Prince', 'Tuxedo', 'Chicken Hearts', 2),
+('Orange Kitty', 'Orange', 'Salmon', 13),
+('Ada', 'Grey', 'Nothing', 13),
+('Blue Diamond', 'White', 'Everything', 3),
+('Eddy Boi', 'Grey', 'Avocado', 4);
