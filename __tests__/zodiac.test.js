ce2ca822e62ac800fe901cbd0121f493d3dbf3e6
Delaney Fogarty <delaney.fk@gmail.com>
2022-06-17 10:37:36 -0700

    test passing for get zodiac by id

--- __tests__/zodiac.test.js
+++ __tests__/zodiac.test.js
@@ -2,10 +2,17 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Zodiac = require('../lib/models/Zodiac');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('/zodiacs/:id should return one gemstone', async () => {
+    const res = await request(app).get('/zodiacs/9');
+    const sag = await Zodiac.getSingleZodiac(9);
+    expect(res.body).toEqual(sag);
   });
 
   it('/zodiacs should return a list of zodiacs', async () => {
